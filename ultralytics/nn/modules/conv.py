2aa709a50c44e4ff17e9a24d1f07ee830a05997e
Kian1208 <97808709+Kian1208@users.noreply.github.com>
2024-04-30 10:53:29 +0200

    print statement

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -52,10 +52,10 @@ class Conv(nn.Module):
 
         # Freeze convolutional layer
         self.conv.requires_grad = False
-
+        print('Conv layer frozen within conv module')
         self.ssf_scale_1.requires_grad = True
         self.ssf_shift_1.requires_grad = True
-
+        print('SSF layer unfrozen within conv module')
 
 
     def forward(self, x):
